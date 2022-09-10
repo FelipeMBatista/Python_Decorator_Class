c0c00a9f5441bcc0ebce7985452da142b6e107cd
Felipe MB <profissional.felipemb@gmail.com>
2022-09-09 21:45:07 -0300

    Added new lines of my study day

--- hello.py
+++ hello.py
@@ -3,11 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>Cute kitten gif</p>' \
+           '<img src="https://media.giphy.com/media/6uMqzcbWRhoT6/giphy.gif" width=700px>'
 
 @app.route('/bye')
 def say_bye():
     return 'Bye!'
 
+@app.route('/<name>')
+def greet(name):
+    return f'Hello {name}!'
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
